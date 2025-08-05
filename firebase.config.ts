645ddaa738632cbb95f01e84dbcfe876814ee55b
Debaraj-stha <dstha221@gmail.com>
2025-08-05 13:53:34 +0545

    adding firebase data

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import firebasedata from "./firevase-data";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDY6mSp8HO9LbRO0i0yKr8FnlAIjamJM5c",
-  authDomain: "react-blog-app-39526.firebaseapp.com",
-  projectId: "react-blog-app-39526",
-  storageBucket: "react-blog-app-39526.firebasestorage.app",
-  messagingSenderId: "868352212029",
-  appId: "1:868352212029:web:3fb058d4adef7122a27e54",
-  measurementId: "G-JNL11G82HG"
+  apiKey: firebasedata.apiKey,
+  authDomain:firebasedata.authDomain,
+  projectId: firebasedata.projectId,
+  storageBucket:firebasedata.storageBucket,
+  messagingSenderId: firebasedata.messagingSenderId,
+  appId: firebasedata.appId,
+  measurementId:firebasedata.measurementId
 };
 
 
