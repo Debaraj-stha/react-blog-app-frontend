7ad8cc8f5eddb2630c65d50c110475870ce42168
Debaraj-stha <dstha221@gmail.com>
2025-08-05 13:33:42 +0545

    updating firebase key

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,17 +8,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDY6mSp8HO9LbRO0i0yKr8FnlAIjamJM5c",
+  authDomain: "react-blog-app-39526.firebaseapp.com",
+  projectId: "react-blog-app-39526",
+  storageBucket: "react-blog-app-39526.firebasestorage.app",
+  messagingSenderId: "868352212029",
+  appId: "1:868352212029:web:3fb058d4adef7122a27e54",
+  measurementId: "G-JNL11G82HG"
 };
 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth(app)
-export { app, firebaseAuth }
+const firebaseAuth=getAuth(app)
+export {app,firebaseAuth}
