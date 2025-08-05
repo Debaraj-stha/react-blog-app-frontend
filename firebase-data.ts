e46210868fd5122210d8a9310b843b78f9379b7c
Debaraj-stha <dstha221@gmail.com>
2025-08-05 15:08:55 +0545

    adding firebase details

---
+++ firebase-data.ts
@@ -0,0 +1,10 @@
+const firebasedata = {
+    apiKey: "AIzaSyDY6mSp8HO9LbRO0i0yKr8FnlAIjamJM5c",
+    authDomain: "react-blog-app-39526.firebaseapp.com",
+    projectId: "react-blog-app-39526",
+    storageBucket: "react-blog-app-39526.firebasestorage.app",
+    messagingSenderId: "868352212029",
+    appId: "1:868352212029:web:3fb058d4adef7122a27e54",
+    measurementId: "G-JNL11G82HG"
+}
+export default firebasedata
--- firebase.config.ts
+++ firebase.config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebasedata from "./firevase-data";
+import firebasedata from "./firebase-data";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
