9f314268bfe6b1adbce46ba565a664426a5da372
Debaraj-stha <dstha221@gmail.com>
2025-08-05 15:12:04 +0545

    updating firebase config

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebasedata from "./firebase-data";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: firebasedata.apiKey,
-  authDomain:firebasedata.authDomain,
-  projectId: firebasedata.projectId,
-  storageBucket:firebasedata.storageBucket,
-  messagingSenderId: firebasedata.messagingSenderId,
-  appId: firebasedata.appId,
-  measurementId:firebasedata.measurementId
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firebaseAuth=getAuth(app)
-export {app,firebaseAuth}
+const firebaseAuth = getAuth(app);
+
+export { app, firebaseAuth };
