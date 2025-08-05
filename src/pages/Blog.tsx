75f4045593660b8b6404440e7d2f92534cc45571
Debaraj-stha <dstha221@gmail.com>
2025-08-05 15:32:54 +0545

    adding meta tag to single blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -20,6 +20,18 @@ const Blog = ({ showAuthor = true, showSimilarBlogs = true, showFeedbackForm = t
             <Helmet>
                 <title>{blog.title} - React Blog App</title>
                 <meta name="keywords" content="react, blog, articles, posts" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" type="image/png" href="./src/assets/blog.png" />
+                <title>React Blog App</title>
+                <meta property="og:title" content="React Blog App" />
+                <meta property="og:description" content="A full-stack blog platform built with React + Node.js + TypeScript." />
+                <meta property="og:image" content="https://react-blog-app-frontend-psi.vercel.app/preview.jpg" />
+                <meta property="og:url" content="https://react-blog-app-frontend-psi.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="React Blog App" />
+                <meta name="twitter:description" content="A full-stack blog platform built with React + Node.js + TypeScript." />
+                <meta name="twitter:image" content="https://react-blog-app-frontend-psi.vercel.app/preview.jpg" />
             </Helmet>
             <SingleBlog blog={blog} showAuthor={showAuthor} showFeedbackForm={showFeedbackForm}
                 showSimilarBlogs={showSimilarBlogs} />
